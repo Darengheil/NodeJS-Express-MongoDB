d41faae2951b356c6fbe707f77893be212ff4655
Einhart <einhvas@gmail.com>
2021-02-06 05:13:22 +0300

    Added dish Router

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,6 +12,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+
+/*
 app.all('/dishes', (req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -26,7 +30,7 @@ app.post('/dishes', (req, res, next) => {
 
 app.put('/dishes', (req, res, next) => {
     res.statusCode = 403;
-    res.end('PUT operation is not supported on the /dishes')
+    res.end('PUT operation is not supported on the /dishes');
 });
 
 app.delete('/dishes', (req, res, next) => {
@@ -50,6 +54,8 @@ app.put('/dishes/:dishId', (req, res, next) => {
 app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 });
+*/
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
