b296bc1bdefde826bd814006fa09cfdc80ab9307
Einhart <einhvas@gmail.com>
2021-02-09 05:14:45 +0300

    added comments inside dishes

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,23 +9,39 @@ connect.then((db) => {
     console.log("Connected correctly to sever");
 
     Dishes.create({
-        name: 'Uthapizza',
+        name: 'Uthappizza',
         description: 'test'
     })
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes); 
-        
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
         return Dishes.remove({});
     })
     .then(() => {
         return mongoose.connection.close();
     })
-    .catch((error) => {
-        console.log(error);
-    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
